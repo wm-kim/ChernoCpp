54f2bb4300a663058bc54201935da362f2014ae7
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-01 15:02:35 +0900

    custom project settings & how to debug in c++

--- Project1/Project1/src/Main.cpp
+++ Project1/Project1/src/Main.cpp
@@ -6,6 +6,16 @@ void Log(const char* message);
 
 int main()
 {
+	int a = 8;
+	a++;
+	const char* string = "Hello";
+	
+	for (int i = 0; i < 5; i++)
+	{
+		const char c = string[i];
+		std::cout << c << std::endl;
+	}
+
 	std::cout << "Hello World!" << std::endl;
 	std::cin.get();
 }
