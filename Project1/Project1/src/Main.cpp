c3155febd5ca6b0e0414ff4b33bdc314be6b4223
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-01 22:05:08 +0900

    const

--- Project1/Project1/src/Main.cpp
+++ Project1/Project1/src/Main.cpp
@@ -4,39 +4,40 @@
 class Entity
 {
 private:
-	std::string m_Name;
-	mutable int m_DebugCount = 0;
+	int m_X, m_Y;
+	// mutable allows you to modify a variable in a const function
+	mutable int var;
 public:
-	const std::string& GetName() const
-	{ 
-		m_DebugCount++;
-		return m_Name; 
+	// const means that the function will not modify the class
+	int GetX() const
+	{
+		var = 2;
+		// m_X = 2; // This will not work because the function is const
+		return m_X;
 	}
 };
 
+void PrintEntity(const Entity& e)
+{
+	// e = Entity(); // This will not work because the function is const
+	// if GetX was not const, this would not work either
+	// because it is not sure that GetX will not modify the class
+	std::cout << e.GetX() << std::endl;
+}
+
 int main()
 {
-	const Entity e;
-	e.GetName();
-
-	int x = 8;
-
-	// doesn't write this lamdba very often
-// 
-	// define capture method, by value
-	// mutable lambda means we can change the value of x, when we capture by value
-	auto f = [=]() mutable
-	{
-		// int y = x; 
-		// y++;
-		// std::cout << y << std::endl;
-		
-		x++; // just going to create local variable 
-		// make more cleaner
-		std::cout << x << std::endl;
-	};
+	const int MAX_AGE = 90;
+	const int* a = new int; // can't change content of the pointer
+	int const* c = new int; // can't change content of the pointer (same as above)
 	
-	// x = 8;
+	int* const b = new int; // can't change the pointer
+
+	const int* const d = new int; // can't change the pointer or the content of the pointer
 	
+	// *a = 2;
+	a = (int*)&MAX_AGE;
+	std::cout << *a << std::endl;
+
 	std::cin.get();
 }
