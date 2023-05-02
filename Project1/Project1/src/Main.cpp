fdb29733196aab31a6ed2b1b3c2e456ac0f004a7
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-02 15:34:59 +0900

    local static

--- Project1/Project1/src/Main.cpp
+++ Project1/Project1/src/Main.cpp
@@ -3,40 +3,30 @@
 #include <memory>
 #include <vector>
 
-// Optimizing the usage of std::vector by avoiding unnecessary copies
-struct Vertex
+// static local variable allows to declare varaible inside function of life time of program
+// however its scope is limited to scope(function, if statement, ...)
+
+void Function()
 {
-	float x, y, z;
+	static int i = 0;
+	i++;
+	std::cout << i << std::endl;
+}
 
-	Vertex(float x, float y, float z)
-		: x(x), y(y), z(z)
+class Singleton
+{
+public:
+	static Singleton& Get()
 	{
+		static Singleton instance;
+		return instance;
 	}
 
-	Vertex(const Vertex& vertex)
-		: x(vertex.x), y(vertex.y), z(vertex.z)
-	{
-		std::cout << "Copied!" << std::endl;
-	}
+	void Hello() { std::cout << "Hello" << std::endl; }
 };
 
 int main()
 {
-	// if the vector has not enough space
-	// it will allocate more space and copy the old data to the new location
-	std::vector<Vertex> vertices; 
-	
-	// reserve the space for the vector
-	vertices.reserve(3);
-
-	vertices.push_back(Vertex(1, 2, 3));
-	vertices.push_back(Vertex(4, 5, 6));
-	vertices.push_back(Vertex(7, 8, 9));
-
-	// whould like to construct the vertex in the vector by using emplace_back
-	vertices.emplace_back(Vertex(1, 2, 3));
-	vertices.emplace_back(Vertex(4, 5, 6));
-	vertices.emplace_back(Vertex(7, 8, 9));
-
+	Singleton::Get().Hello();
 	std::cin.get();
 }
