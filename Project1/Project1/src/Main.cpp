dd6af2829f05c1b026747dab482832943811214c
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-02 13:30:03 +0900

    this keyword

--- Project1/Project1/src/Main.cpp
+++ Project1/Project1/src/Main.cpp
@@ -1,59 +1,34 @@
 #include <iostream> 
 #include <string>
 
-// using operator overloading 
-// ex. Add -> +
-// use & to push the variable into dataset
+void PrintEntity(const Entity& e);
 
-struct Vector2
+class Entity 
 {
-	float x, y;
-	Vector2(float x, float y) : x(x), y(y) {}
-	Vector2 Add(const Vector2& other) const
+public:
+	int x, y;
+	
+	Entity(int x, int y)
 	{
-		// code styles
-		return Vector2(x + other.x, y + other.y);
-		// return *this + other;
-		// return operator+(other);
+		this->x = x;
+		this->y = y;
+
+		Entity& e = *this;
+
+		PrintEntity(this);
+
+		// avoid this, freeing memory from member function
+		delete this; 
 	}
 
-	Vector2 Multiply(const Vector2& other) const
+	int GetX() const
 	{
-		return Vector2(x * other.x, y * other.y);
-	}
-
-	Vector2 operator+(const Vector2& other) const
-	{
-		return Add(other);
-	}
-
-	Vector2 operator*(const Vector2& other) const
-	{
-		return Multiply(other);
-	}
-
-	bool operator==(const Vector2& other) const
-	{
-		return x == other.x && y == other.y;
+		const Entity* e = this;
+		return x;
 	}
 };
 
-std::ostream& operator<<(std::ostream& stream, const Vector2& other)
-{
-	stream << other.x << ", " << other.y;
-	return stream;
-}
-
 int main()
 {
-	Vector2 position = { 4.0f, 4.0f };
-	Vector2 speed = { 0.5f, 1.5f };
-	Vector2 powerup = { 1.1f, 1.1f };
-
-	Vector2 result = position.Add(speed);	
-	Vector2 result2 = position + speed * powerup;
-
-	std::cout << result << std::endl;
-	
 	std::cin.get();
 }
