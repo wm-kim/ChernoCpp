9e455409e4aa55502bebe7b25fa083d01db2f131
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-03 22:22:44 +0900

    lambda usecases

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,45 +1,40 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <functional>
 
-// function pointer comes from C
-// function is just cpu instructions. somewhere in binary (executable file)
-// function pointer finds the address of the function in the binary
+// lambda : ways to create function without having to pyhsically create function
+// quick disposable function
 
-void HelloWorld(int a)
+// how to use, when to use?
+// when you need to create a function that is only used once
+
+void Foreach(const std::vector<int>& array, const std::function<void(int)>& func)
 {
-	std::cout << "Hello World Value : " << a << std::endl;
-}
-
-void PrintValue(int value)
-{
-	std::cout << "Value : " << value << std::endl;
-}
-
-void Foreach(const std::vector<int>& values, void(*func)(int))
-{
-	for (int value : values)
+	for (int value : array)
 		func(value);
 }
 
 int main()
 {
-	// this is confusing, so people tend to use auto or typedef 
-	// void(*function)() = HelloWorld;
-	typedef void(*HelloWorldFunction)(int);
-	
-	auto function = HelloWorld; // implicit convertion (& not needed)
-	HelloWorldFunction function2 = HelloWorld;
+	std::vector<int> values = { 1, 5, 4, 2, 3 };
 
-	function(8);
+	// lambda 
+	// C++ lambda expressions : https://en.cppreference.com/w/cpp/language/lambda
 
-	// example of why you use function pointer.
-	// tell function what to do
-	std::vector<int> values = { 1, 5, 4, 2 ,3 };
-	Foreach(values, PrintValue);
+	// what is called "capturing"
+	// pass outside variable into lambda function (by refernce or by value)
+	// [=] : capture all outside variable by value
+	// [&] : capture all outside variable by reference
+	int a = 5;
 
-	// lambda
-	Foreach(values, [](int value) { std::cout << "Value : " << value << std::endl; });
+	auto lambda = [=](int value) { std::cout << "Value: " << value << std::endl; };
+	auto lambda2 = [=](int value) mutable { a = 5; std::cout << "Value: " << value << std::endl; };
+	Foreach(values, lambda);
+
+	// typicall usecase
+	auto it = std::find_if(values.begin(), values.end(), [](int value) { return value > 3; });
+	std::cout << *it << std::endl;
 	
 	std::cin.get();
 }
