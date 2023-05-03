4674f2ecaa55921120c5789999995a00449e170f
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-03 21:16:38 +0900

    how to use 'auto' keyword properly in c++

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,35 +1,73 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
 
-// don't like overusing macros, its get very hard to read for the first time
+// Auto keyword
+// If I have auto, do I really need to every write any types ? 
+// comes end to matter of style.
 
-#define WAIT std::cin.get() // don't do this, don't use preprocesser this way
+// server side 
+std::string GetName() {}
 
-// or use in memory allocation in debug build to know how many bytes are allocated
-// from which file and which line. if you replace new keyword into custom keyword
+class Device {};
 
-#if 0
-// how to use marcro
-// #define PR_DEBUG 1
+class DeviceManager
+{
+private:
+	std::unordered_map<std::string, std::vector<Device*>> m_Devices;
+public:
+	const std::unordered_map<std::string, std::vector<Device*>>& GetDevices() const
+	{
+		return m_Devices;
+	}
+};
 
-#if PR_DEBUG == 1
-#define LOG(x) std::cout << x << std::endl
-#elif defined(PR_RELEASE)
-#define LOG(x) // will automatically remove this line
-#endif
-
-#endif
-
-// don't put space after backslash
-#define MAIN int main() \
-{\
-	std::cin.get(); \
+// C++14
+auto GetName2()
+{
+	return "Cherno";
 }
-//
-//int main()
-//{
-//	WAIT;
-//	LOG("Hello World");
-//}
 
-MAIN
+// C++11
+auto GetName3() -> const char*
+{
+	return "Cherno";
+}
+
+int main()
+{
+	// std::string name = GetName();
+	// in client side, code remains the same
+	// but this could be a bad thing, because the api can change
+	
+	// for these cases recommand not to use auto
+	auto name = GetName();
+	name.size();
+
+	// In this case, auto is useful
+	std::vector<std::string> strings;
+	strings.push_back("Apple");
+	strings.push_back("Orange");
+
+
+	// iterator is not used in c++11
+	// for (std::vector<std::string>::iterator it = strings.begin(); it != strings.end(); it++)
+	for (auto it = strings.begin(); it != strings.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	using DeviceMap = std::unordered_map<std::string, std::vector<Device*>>;
+	// typedef std::unordered_map<std::string, std::vector<Device*>> DeviceMap;
+	
+	DeviceManager dm;
+	const DeviceMap& devices = dm.GetDevices();
+	// keep in mind that we have to use reference here (not to make a copy)
+	const auto& devices2 = dm.GetDevices();
+
+	// auto keywords also used in trailing return types,
+	// also can use function decide ruturn type
+	
+	std::cin.get();
+}
