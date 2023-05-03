175ea6ac3bdc8e7c51775b1227931dbcb2d3b730
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-04 08:35:04 +0900

    why don't use namespace std

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,40 +1,35 @@
 #include <iostream>
-#include <array>
-#include <vector>
-#include <functional>
 
-// lambda : ways to create function without having to pyhsically create function
-// quick disposable function
+// why don't use namespace std;
+// 1. want to know what I am using
+// 2. avoid possible errors in the future
+// don't use namespace in header file (never do that)
+// just use inside the function or small scope
 
-// how to use, when to use?
-// when you need to create a function that is only used once
-
-void Foreach(const std::vector<int>& array, const std::function<void(int)>& func)
-{
-	for (int value : array)
-		func(value);
+namespace apple {
+	// implicit conversion
+	void print(const std::string& text)
+	{
+		std::cout << text << std::endl;
+	}
 }
+
+namespace orange {
+	// don't require any kind of conversion
+	void print(const char* text)
+	{
+		std::string temp = text;
+		std::reverse(temp.begin(), temp.end());
+		std::cout << text << std::endl;
+	}
+}
+
+using namespace apple;
+using namespace orange;
 
 int main()
 {
-	std::vector<int> values = { 1, 5, 4, 2, 3 };
+	print("Hello"); // olleh
 
-	// lambda 
-	// C++ lambda expressions : https://en.cppreference.com/w/cpp/language/lambda
-
-	// what is called "capturing"
-	// pass outside variable into lambda function (by refernce or by value)
-	// [=] : capture all outside variable by value
-	// [&] : capture all outside variable by reference
-	int a = 5;
-
-	auto lambda = [=](int value) { std::cout << "Value: " << value << std::endl; };
-	auto lambda2 = [=](int value) mutable { a = 5; std::cout << "Value: " << value << std::endl; };
-	Foreach(values, lambda);
-
-	// typicall usecase
-	auto it = std::find_if(values.begin(), values.end(), [](int value) { return value > 3; });
-	std::cout << *it << std::endl;
-	
 	std::cin.get();
 }
