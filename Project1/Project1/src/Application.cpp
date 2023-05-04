b6fb5e46c77d86a3b2ba2d86365d9ff3360b7c79
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-04 10:32:11 +0900

    virtual destructor

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,70 +1,31 @@
 #include <iostream>
 
-// Union can only have one member, closed linked to type punning
-// useful when you want different names for the same memory location
-
-struct Vector2
+class Base
 {
-	float x, y;
+public:
+	Base() { std::cout << "Base Constructor" << std::endl; }
+	virtual ~Base() { std::cout << "Base Destructor" << std::endl; }
 };
 
-struct Vector4
+class Derived : public Base
 {
-	union 
-	{
-		// anonymous struct
-		struct 
-		{
-			float x, y, z, w;
-		};
-		struct
-		{
-			// a is same memory location as x
-			// b is same memory location as y
-			Vector2 a, b;
-		};
-	};
-
-	// this creates whole object 
-	// Vector2 GetA() { return Vector2(); }
-
-	// Type punning
-	/*Vector2& GetA()
-	{
-		return *(Vector2*)&x;
-	}*/
+public:
+	Derived() { m_Array = new int[5];  std::cout << "Derived Constructor" << std::endl; }
+	~Derived() { delete[] m_Array; std::cout << "Derived Destructor" << std::endl; }
+private:
+	int* m_Array;
 };
-
-void PrintVector2(const Vector2& vector)
-{
-	std::cout << vector.x << ", " << vector.y << std::endl;
-}
 
 int main()
 {
-	struct Union
-	{
-		union
-		{
-			float a;
-			int b;
-		};
-	};
+	Base* base = new Base();
+	delete base;
+	std::cout << "---------------------" << std::endl;
+	Derived* derived = new Derived();
+	delete derived;
+	std::cout << "---------------------" << std::endl;
+	Base* poly = new Derived();
+	delete poly;
 
-	Union u;
-	u.a = 2.0f;
-	std::cout << u.a << ", " << u.b << std::endl;
-
-	{
-		Vector4 vector = { 1.0f, 2.0f, 3.0f, 4.0f };
-		
-		PrintVector2(vector.a);
-		PrintVector2(vector.b);
-		vector.z = 500.0f;
-		std::cout << "-----------------" << std::endl;
-		PrintVector2(vector.a);
-		PrintVector2(vector.b);
-	}
-	
 	std::cin.get();
 }
