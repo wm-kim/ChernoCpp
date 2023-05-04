7fdf399c77ffe1e7255aadfe4942eaa3c83dd8f7
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-04 12:26:40 +0900

    updated read.me

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,52 +1,11 @@
 #include <iostream>
 
-// C.C++ style type casting; should learn from experience
-
-class Base
-{
-public:
-	Base() {}
-	~Base() {}
-};
-
-class Derived : public Base
-{
-public:
-	Derived() {}
-	~Derived() {}
-};
-
-class AnotherClass : public Base
-{
-public:
-	AnotherClass() {}
-	~AnotherClass() {}
-};
-
-// C++ style type casting (static cast)
-// these can do whatever C style casting can do (+alpha)
-// not adding new fucntionality but its syntax suger
-// 1. static_cast : static cast, compile time check
-// 2. dynamic_cast : perform a check and may return null if it fails
-// 3. const_cast : adding or removing const 
-// 4. reinterpret_cast : don't want to convert anything, just want reinterpreting memory
+// conditional breakpoint : tell the debugger, to stop when a condition is met
+// action breakpoint : tell the debugger, to do something when a condition is met
 
 int main()
 {
-	// C style type casting
-	double value = 5.25;
-	double a = (int)value + 5.3;
-
-	// static cast
-	double s = static_cast<int>(value) + 5.3;
-	// double s = static_cast<AnotherClass*>(&value) + 5.3; - error (compile time checking)
 	
-	// dynamic_cast
-	Derived* derived = new Derived();
-	Base* base = derived;
-	// atempt conversion
-	AnotherClass* ac = static_cast<AnotherClass*>(base);
-	if (ac) {}
 
 	std::cin.get();
 }
--- README.md
+++ README.md
@@ -2,3 +2,11 @@
  
 Cherno C++ Course 학습 내용 정리
 url : https://www.youtube.com/watch?v=18c3MTX0PK0&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=1
+
+this course teaches you how to be a grood c++ programmer 
+writing readable and managable code on Production Level.
+
+There are also lots of tips & practices to learn to optimize, write fast c++ code.
+
+This is my notes of this course.
+url : https://pollen-tugboat-e59.notion.site/Cherno-C-Course-Notes-6eec4ae94e4b4a78acb73247c8d15e7e
