af7fa056cd28a6b8353760ccc3e300f250d128e7
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-05 08:37:31 +0900

    std any c++17

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,52 +1,26 @@
 #include <iostream>
-#include <variant>
-#include <string>
-#include <optional>
-
-// good to know what when wrong instead of saying data is not present
-std::optional<std::string> ReadFileAsString()
-{
-	return {};
-}
-
-enum class ErrorCode
-{
-	None = 0, NotFound = 1, NoAccess = 2
-};
-
-std::variant<std::string, ErrorCode> ReadFileAsString2()
-{
-	return {};
-}
-
-// std::variant 
-// allow us to not have to worry about the type of the data
-// list all the type it could be, and later decide which one it is
+#include <any>
 
 int main()
 {
-	std::variant<std::string, int> data;
-	// union size is the size of the largest type
-	// but variant size is the size of all the types combined
-	// union is more efficent but variant is more type safe
-	
-	data = "Cherno";
-	std::cout << std::get<std::string>(data) << std::endl;
-	data.index(); // 0 
-	if (auto* value = std::get_if<std::string>(&data)) // returns a pointer to the data if it is a string, otherwise returns nullptr
-	{
-		std::string& v = *value;
-	}
-	else
-	{
-
-	}
-	
+	std::any data;
 	data = 2;
-	data.index(); // 1
-	 
-	std::cout << std::get<int>(data) << std::endl;
-	// std::cout << std::get<std::string>(data) << std::endl; - exception
+	data = "Cherno";
+	data = std::string("Cherno");
+	
+	// will get exception if we try to cast to wrong type
+	std::string& string = std::any_cast<std::string&>(data);
+
+	// can store any type while std::variant have to list all types
+	// the fact that std::variant requires to list out all types is a good thing - type safe
+	// variant is just typesafe union
+
+	// std::any is for small data types it just store as a union
+	// when we store a big data type it will dynamically allocate memory (more than 32 bytes)
+	// (not good performance)
+	// std::variant performs faster if you deal with larger data
+	
+	// when to use std::any? - bit of a useless
 	
 	std::cin.get();
 }
