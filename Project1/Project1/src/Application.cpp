b6e327e4a98c24372e048409245ffda280fa3a01
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-04 13:57:27 +0900

    c++17 struct bindings

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,77 +1,40 @@
 #include <iostream>
-#include <memory>
-#include <chrono>
+#include <string>
+#include <tuple>
 
-#include <array>
-
-// when you trying to benchmarking do work that acutally needs to be done
-// compiler can optimize your code
-
-class Timer
+/*
+struct Person
 {
-public:
-	Timer()
-	{
-		m_StartTimePoint = std::chrono::high_resolution_clock::now();
-	}
-	~Timer()
-	{
-		Stop();
-	}
-
-	void Stop()
-	{
-		auto endTimePoint = std::chrono::high_resolution_clock::now();
-
-		auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimePoint).time_since_epoch().count();
-		auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
-	
-		auto duration = end - start;
-		double ms = duration * 0.001;
-		
-		std::cout << duration << "us (" << ms << "ms)\n";
-	}	
-private:
-	std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimePoint;
+	std::string Name;
+	int Age;
 };
+*/
+
+
+// Structured bindings C++17
+std::tuple<std::string, int> CreatePerson()
+{
+	return { "John", 25 };
+}
 
 int main()
 {
-	int value = 0;
 	{
-		Timer timer;
-		for (int i = 0; i < 1000000; i++)
-			value += 2;
+		std::tuple<std::string, int> person = CreatePerson();
+		// auto person = CreatePerson();
+		std::string& name = std::get<0>(person);
+		int age = std::get<1>(person);
 	}
 
-	std::cout << value << std::endl;
-
-	// compare performance of shared pointer vs unique pointer
-	struct Vector2 { float x, y; };
-
-	// compile and test in release mode because release will be shipped to the user
-	std::cout << "Make shared\n";
 	{
-		Timer timer;
-		std::array<std::shared_ptr<Vector2>, 1000> sharedPtrs;
-		for (int i = 0; i < sharedPtrs.size(); i++)
-			sharedPtrs[i] = std::make_shared<Vector2>();
+		std::string name;
+		int age;
+		std::tie(name, age) = CreatePerson();
 	}
-	
-	std::cout << "New Shared\n";
+
+	// C++17
 	{
-		Timer timer;
-		std::array<std::shared_ptr<Vector2>, 1000> sharedPtrs;
-		for (int i = 0; i < sharedPtrs.size(); i++)
-			sharedPtrs[i] = std::shared_ptr<Vector2>(new Vector2());
-	}
-	
-	std::cout << "Make unique\n";
-	{
-		Timer timer;
-		std::array<std::unique_ptr<Vector2>, 1000> uniquePtrs;
-		for (int i = 0; i < uniquePtrs.size(); i++)
-			uniquePtrs[i] = std::make_unique<Vector2>();
+		auto [name, age] = CreatePerson();
 	}
 
 	std::cin.get();
