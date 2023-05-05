ec45f432fa37c2c08399f76896a5085e404ff28b
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-05 11:16:08 +0900

    lvalue & rvalue

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,57 +1,28 @@
 #include <iostream>
-#include <string>
-#include <memory>
 
-// How to Track memory allocation
-// Trace back in callstack where the memory allocation is coming from
-// VS has memory allcation tracking profile tools
+// lvalue & rvalue
 
-// quick way to track memory allocation
-struct AllocationMatrics
+// lvalue reference taking lvalue unless its const
+void PrintName(const std::string& name)
 {
-	uint32_t TotalAllocated = 0;
-	uint32_t TotalFreed = 0;
-
-	uint32_t CurrentUsage() { return TotalAllocated - TotalFreed; }
-};
-
-static AllocationMatrics s_AllocationMatrics;
-
-void* operator new (size_t size)
-{
-	s_AllocationMatrics.TotalAllocated += size;
-	// std::cout << "Allocating " << size << " bytes\n";
-	// breakpoint here
-	return malloc(size);
+	std::cout << "[lvalue] " << name << std::endl;
 }
 
-void operator delete (void* memory, size_t size)
+// rvalue reference only taking rvalue
+// can steal the resources from the temporary value - help with optimization
+void PrintName(std::string&& name)
 {
-	s_AllocationMatrics.TotalFreed += size; 
-	// std::cout << "Freeing " << size << " bytes\n";
-	free(memory);
+	std::cout << "[rvalue] " << name << std::endl;
 }
-
-static void PrintMemoryUsage()
-{
-	std::cout << "Memory Usage: " << s_AllocationMatrics.CurrentUsage() << " bytes\n";
-}
-
-struct Object
-{
-	int x, y, z;
-};
 
 int main() 
 {
-	PrintMemoryUsage();
-	std::string string = "Cherno";
-	PrintMemoryUsage();
-	{
-		std::unique_ptr<Object> obj = std::make_unique<Object>();
-		PrintMemoryUsage();
-	}
-	PrintMemoryUsage();
+	std::string firstName = "Yan";
+	std::string lastName = "Chernikov";
 
+	std::string fullname = firstName + lastName;
+
+	PrintName(firstName); // lvalue
+	PrintName(firstName + lastName); // rvalue
 	std::cin.get();
 }
