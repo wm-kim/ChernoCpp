c443c6f20d0691a6ec4cbce18ce0c5e35ac9d968
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2023-05-05 14:08:25 +0900

    making data structure - array c++

--- Project1/Project1/src/Application.cpp
+++ Project1/Project1/src/Application.cpp
@@ -1,110 +1,55 @@
 #include <iostream>
+#include <array>
 
-/*
-std::move & move assignment operator
-
-
-*/
-
-class String 
+// you can use int but usually in c++ use size_t interally
+template<typename T, size_t S>
+class Array
 {
 public:
-	String() = default;
-	String(const char* string)
+	/*
+	Array(int size)
 	{
-		std::cout << "Created!\n";
-		m_Size = strlen(string);
-		m_Data = new char[m_Size];
-		memcpy(m_Data, string, m_Size);
+		// let us allocate memory on stack - can allocate on runtime
+		// compiler doens't know how big is size is, lack of compiler optimization
+		// but can be useful in some cases
+		// m_Data = (int*)alloca(size * sizeof(int));
 	}
-	
-	String(const String& other)
+	*/
+	// int Size() const { return 5; }
+	constexpr size_t Size() const { return 5; }
+	T& operator[](size_t index) 
 	{
-		std::cout << "Copied!\n";
-		m_Size = other.m_Size;
-		m_Data = new char[m_Size];
-		memcpy(m_Data, other.m_Data, m_Size);
-	}
-
-	String(String&& other) noexcept
-	{
-		std::cout << "Moved!\n";
-		m_Size = other.m_Size;
-		m_Data = other.m_Data;
-		other.m_Size = 0;
-		other.m_Data = nullptr;
-	}
-
-	// move assignment operator 
-	// we are not constructing new object, we need to overwrite	the current object;
-	// we need to delete the current data and copy the data from the other object
-	String& operator=(String&& other) noexcept
-	{
-		std::cout << "Moved!\n";
-		// check for self assignment
-		if (this != &other)
+		/*if (!(index < S)) - don't include in release mode
 		{
-			delete[] m_Data;
-			m_Size = other.m_Size;
-			m_Data = other.m_Data;
-			other.m_Size = 0;
-			other.m_Data = nullptr;
-		}
-		return *this;
+			__debugbreak();
+		}*/
+		return m_Data[index]; 
 	}
-	
-	~String()
-	{
-		std::cout << "Destroyed!\n";
-		delete[] m_Data;
-	}
-
-	void Print()
-	{
-		for (uint32_t i = 0; i < m_Size; i++)
-			std::cout << m_Data[i];
-	}
+	const T& operator[](size_t index) const { return m_Data[index]; }  // const verison
+		T* Data() { return m_Data; }
+		const T* Data() const { return m_Data; }
 private:
-	char* m_Data;
-	uint32_t m_Size;
-};
-
-class Entity
-{
-public:
-	Entity(const String& name)
-		: m_Name(name) {}
-	
-	Entity(String&& name)
-		: m_Name(std::move(name)) {}
-
-	void PrintName() 
-	{
-		m_Name.Print();
-	}
-	
-private:
-	String m_Name;
+	T m_Data[S];
 };
 
 int main() 
 {
-	String string = "Hello";
-	// make it into temperary object, like a cast 
-	 
-	// String dest((String&&)string);
-	// more elegant and flexible way : compile time figures out what type is coming in
-	String dest = std::move(string);
+	Array<int, 5> data; 
 
-	// assignment operator only get calls when object is already created
+	// take andantage that in compile time know the size
+	static_assert(data.Size() < 10, "Size is too large!"); // needs constexpr
 
-	String apple = "Apple";
-	String dest;
+	Array<std::string, data.Size()> newArray;
 
-	// dest = apple; error because we don't have copy assignment operator
-	dest = std::move(apple); // dest.=(std::move(apple));
-	// move constructor is different from move assignment operator
-	
-	
+	for (int i = 0; i < data.Size(); i++)
+	{
+		// data[i] = 2; // need index operator return reference type
+		std::cout << data[i] << std::endl;
+	}
+
+	memset(data.Data(), 0, data.Size() * sizeof(int));
+	// since this is stack allocated its gaurenteed to be contiguous
+	memset(&data[0], 0, data.Size() * sizeof(int));
+
 	std::cin.get();
 }
